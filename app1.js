9829cb4c2b692441879d8336729e2dad5f59d256
NoRae <kektus09@gmail.com>
2023-09-15 02:12:51 +0600

    initial commit

---
+++ app1.js
@@ -0,0 +1,36 @@
+//task1 start
+var userAnswer = prompt('Введите ваше имя')
+console.log('Меня зовут ' + userAnswer);
+
+var userAnswer = prompt('Введите ваш возраст')
+console.log('Мне ' + userAnswer + ' лет');
+
+var task1 = 20
+var userAnswer = prompt('Решите пример: 16 + 4 = ?');
+console.log('Пример 1: 16 + 4 = ' + task1 + ' Ваш ответ: ' + userAnswer);
+
+var task2 = 88
+var userAnswer = prompt('Решите пример: 120 - 32 = ?');
+console.log('Пример 2: 120 - 32 = ' + task2 + ' Ваш ответ: ' + userAnswer);
+
+var task3 = 36
+var userAnswer = prompt('Решите пример: 6 * 6 = ?');
+console.log('Пример 3: 6 * 6 = ' + task3 + ' Ваш ответ: ' + userAnswer);
+
+var task4 = 794
+var userAnswer = prompt('Решите пример: 1588 / 2 = ?');
+console.log('Пример 3: 1588 / 2 = ' + task4 + ' Ваш ответ: ' + userAnswer);
+
+var task5 = 10.5
+var userAnswer = prompt('Решите пример: 21 / 2 = ?');
+console.log('Пример 3: 21 / 2 = ' + task5 + ' Ваш ответ: ' + userAnswer);
+//task1 complete
+
+//task2 start 
+var X = 12
+var Y = 5
+var Z = 3
+var res = (X + Y + Z) / 3;
+var userAnswer = prompt('Вычислите среднее арифметическое XYZ, если Х = 12;  Y = 5;  Z = 3 ' +  '.' + ' Введите свой ответ:')
+alert('Среднее арифметическое XYZ: ' + res)
+//task2 complete
